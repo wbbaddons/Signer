9407dc80223ca35f62787117a4cc5022eca0a6d0
Tim Düsterhus <timwolla@bastelstu.be>
2013-09-24 20:31:20 +0200

    Add Signer and SignerTest

---
+++ file/util/Signer.class.php
@@ -0,0 +1,99 @@
+<?php
+namespace wcf\util;
+
+/**
+ * Signs data with the WCF secret.
+ * 
+ * @author 	Tim Düsterhus
+ * @copyright	2013 Tim Düsterhus
+ * @license	BSD 3-Clause License <http://opensource.org/licenses/BSD-3-Clause>
+ * @package	be.bastelstu.wcf.sso
+ * @subpackage	action
+ */
+final class Signer {
+	/**
+	 * Signs the given value with the WCF secret.
+	 * 
+	 * @param	string	$value
+	 * @return	string
+	 */
+	public static function getSignature($value) {
+		if (SIGNER_SECRET === '') throw new \wcf\system\exception\SystemException('Empty SIGNER_SECRET, aborting');
+		
+		return hash_hmac('sha1', $value, SIGNER_SECRET);
+	}
+	
+	/**
+	 * Creates a signed string.
+	 * 
+	 * @param	string	$value
+	 * @return	string
+	 */
+	public static function createSignedString($value) {
+		return self::getSignature($value).'-'.base64_encode($value);
+	}
+	
+	/**
+	 * Returns whether the given string is properly signed.
+	 * 
+	 * @param	string	$string
+	 * @return	boolean
+	 */
+	public static function validateSignedString($string) {
+		$parts = explode('-', $string, 2);
+		if (count($parts) !== 2) return false;
+		list($signature, $value) = $parts;
+		$value = base64_decode($value);
+		
+		return PasswordUtil::secureCompare($signature, self::getSignature($value));
+	}
+	
+	/**
+	 * Returns the value of a signed string, after
+	 * validating whether it is properly signed.
+	 * 
+	 * - Returns null if the string is not properly signed.
+	 * 
+	 * @param	string	$string
+	 * @return	null
+	 */
+	public static function getValueFromSignedString($string) {
+		if (!self::validateSignedString($string)) return null;
+		
+		$parts = explode('-', $string, 2);
+		return base64_decode($parts[1]);
+	}
+	
+	/**
+	 * Sets a signed cookie.
+	 * 
+	 * @see		\wcf\util\HeaderUtil
+	 */
+	public static function setSignedCookie($name, $value = '', $expire = 0) {
+		HeaderUtil::setCookie($name, self::getSignedString($value), $expire);
+	}
+	
+	/**
+	 * Returns the value of the cookie with the given name, after
+	 * validating whether it is properly signed.
+	 * 
+	 * - Returns null if the cookie is not properly signed, or does not exist.
+	 * - Unsets the cookie if it is not properly signed.
+	 * 
+	 * @param	string	$name
+	 * @return	string
+	 */
+	public static function getSignedCookie($name) {
+		if (!isset($_COOKIE[$name])) return null;
+		$value = self::getValueFromSignedString($_COOKIE[$name]);
+		
+		if ($value === null) {
+			unset($_COOKIE[$name]);
+			HeaderUtil::setCookie($name, '', -1337);
+		}
+		
+		return $value;
+	}
+	
+	private function __construct() { }
+}
---
+++ test/SignerTest.php
@@ -0,0 +1,90 @@
+<?php
+namespace wcf\util {
+	class PasswordUtil {
+		public function secureCompare($a, $b) {
+			$a = (string) $a;
+			$b = (string) $b;
+			
+			return $a === $b;
+		}
+	}
+}
+
+namespace {
+	require_once __DIR__.'/../file/util/Signer.class.php';
+	require_once 'PHPUnit/Framework/TestCase.php';
+	use \wcf\util\Signer;
+	
+	/**
+	 * Testcase for signer
+	 * 
+	 * @author 	Tim Düsterhus
+	 * @copyright	2013 Tim Düsterhus
+	 * @license	BSD 3-Clause License <http://opensource.org/licenses/BSD-3-Clause>
+	 * @package	be.bastelstu.wcf.signer
+	 */
+	class SignerTest extends PHPUnit_Framework_TestCase {
+		public static function setUpBeforeClass() {
+			define('SIGNER_SECRET', 'secret');
+		}
+		
+		/**
+		 * Tests Signer::getSignature()
+		 */
+		public function testGetSignature() {
+			$this->assertEquals('cd33db5a64e8a2185659da6f09dc78676bc5a19a', Signer::getSignature('a'));
+			$this->assertEquals('f72bf6ad61628dec3c2bc6b9530f63c51244e77b', Signer::getSignature('b'));
+			
+		}
+		
+		/**
+		 * Tests Signer::createSignedString()
+		 */
+		public function testCreateSignedString() {
+			$this->assertEquals('cd33db5a64e8a2185659da6f09dc78676bc5a19a-YQ==', Signer::createSignedString('a'));
+			$this->assertEquals('f72bf6ad61628dec3c2bc6b9530f63c51244e77b-Yg==', Signer::createSignedString('b'));
+		}
+		
+		/**
+		 * Tests Signer::validateSignedString()
+		 */
+		public function testValidateSignedString() {
+			$this->assertTrue(Signer::validateSignedString('cd33db5a64e8a2185659da6f09dc78676bc5a19a-YQ=='));
+			$this->assertTrue(Signer::validateSignedString('f72bf6ad61628dec3c2bc6b9530f63c51244e77b-Yg=='));
+			
+			$this->assertTrue(Signer::validateSignedString(Signer::createSignedString('a')));
+			$this->assertTrue(Signer::validateSignedString(Signer::createSignedString('b')));
+			
+			$this->assertFalse(Signer::validateSignedString(null));
+			$this->assertFalse(Signer::validateSignedString(false));
+			$this->assertFalse(Signer::validateSignedString(1));
+			$this->assertFalse(Signer::validateSignedString('x'));
+			$this->assertFalse(Signer::validateSignedString(''));
+			$this->assertFalse(Signer::validateSignedString('-'));
+			$this->assertFalse(Signer::validateSignedString('x-y'));
+			$this->assertFalse(Signer::validateSignedString('-y'));
+			$this->assertFalse(Signer::validateSignedString('x-'));
+		}
+		
+		/**
+		 * Tests Signer::getValueFromSignedString()
+		 */
+		public function testGetValueFromSignedString() {
+			$this->assertEquals('a', Signer::getValueFromSignedString('cd33db5a64e8a2185659da6f09dc78676bc5a19a-YQ=='));
+			$this->assertEquals('b', Signer::getValueFromSignedString('f72bf6ad61628dec3c2bc6b9530f63c51244e77b-Yg=='));
+			
+			$this->assertEquals('a', Signer::getValueFromSignedString(Signer::createSignedString('a')));
+			$this->assertEquals('b', Signer::getValueFromSignedString(Signer::createSignedString('b')));
+			
+			$this->assertNull(Signer::getValueFromSignedString(null));
+			$this->assertNull(Signer::getValueFromSignedString(false));
+			$this->assertNull(Signer::getValueFromSignedString(1));
+			$this->assertNull(Signer::getValueFromSignedString('x'));
+			$this->assertNull(Signer::getValueFromSignedString(''));
+			$this->assertNull(Signer::getValueFromSignedString('-'));
+			$this->assertNull(Signer::getValueFromSignedString('x-y'));
+			$this->assertNull(Signer::getValueFromSignedString('-y'));
+			$this->assertNull(Signer::getValueFromSignedString('x-'));
+		}
+	}
+}
